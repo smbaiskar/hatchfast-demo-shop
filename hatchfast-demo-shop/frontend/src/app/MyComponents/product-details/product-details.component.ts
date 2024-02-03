08bfac3c319e6242b3c5610c09bd585f50dfc9d5
Raj kalshetty <rajkalshetty6007@gmail.com>
2024-02-03 10:03:38 +0530

    Product details page added

--- hatchfast-demo-shop/frontend/src/app/MyComponents/product-details/product-details.component.ts
+++ hatchfast-demo-shop/frontend/src/app/MyComponents/product-details/product-details.component.ts
@@ -2,10 +2,17 @@ import { Component, Input, OnInit } from '@angular/core';
 import { Product } from '../../models/product';
 import { AppHelper } from '../shared/app.helper';
 import { MatCardModule } from '@angular/material/card';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 import { CommonModule } from '@angular/common';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatTooltipModule } from '@angular/material/tooltip'; 
+
+import { MatButtonModule } from '@angular/material/button';
+import { MatDividerModule }  from '@angular/material/divider';
+import { MatIconModule } from '@angular/material/icon';
+import { RouterModule } from '@angular/router';
+
+
 
 
 
@@ -15,7 +22,7 @@ import { MatTooltipModule } from '@angular/material/tooltip';
 @Component({
   selector: 'app-product-details',
   standalone: true,
-  imports: [MatCardModule,CommonModule,MatToolbarModule,MatTooltipModule],
+  imports: [MatCardModule,CommonModule,MatToolbarModule,MatTooltipModule,RouterLink,MatButtonModule, MatDividerModule, MatIconModule, RouterModule],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.css'
 })
